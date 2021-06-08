8debd882e2efede47407c556b3ac87f262bd71b0
smashfakuddin <smashfakuddin.anthro.cou@gmail.com>
2021-06-08 19:41:31 +0600

    another commit

--- index.js
+++ index.js
@@ -9,44 +9,26 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const port = 4000;
 const app = express();
 
-
 app.use(cors());
 app.use(bodyParser.json());
-
-
-
-
-
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
     const productCollection = client.db("freshValley").collection("products");
     console.log('db connected')
     app.get("/products", (req, res) => {
-       productCollection.find({})
-       .toArray((err,documents)=>{
-           res.send(documents);
-       })
+        productCollection.find({})
+            .toArray((err, documents) => {
+                res.send(documents);
+            })
     })
 
-
 });
-
 
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
-
-
-
-
-
-
-
-
-
-
 
 app.listen(port, () => {
     console.log(`I am listening to ${port}`)
